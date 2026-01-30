0c9941c42fa90fb08c80277656873b73a23083a1
Greg Sypek <greg.sypek@gmail.com>
2026-01-30 14:00:47 +0100

    Add new layout with sidebar, header and footer

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+// app/(root)/layout.tsx
+import Aside from "@/components/Aside";
+import Header from "@/components/Header";
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<div className="flex h-screen overflow-hidden bg-slate-50">
+			{/* Sidebar*/}
+			<Aside />
+			<div className="flex flex-col flex-1 min-w-0">
+				<Header />
+				{/* scrollable main part */}
+				<main className="flex-1 overflow-y-auto p-4 md:p-8">
+					<div className="max-w-7xl mx-auto">{children}</div>
+				</main>
+			</div>
+		</div>
+	);
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,63 @@
+// (root)/page.tsx
+
+import AssetsTable from "../portfel/components/AssetsTable";
+import { mockAssets } from "@/lib/constants";
+import ComparisonBars from "../portfel/components/ComparisonBars";
+import PortfolioTableBeauty from "../portfel/components/PortfolioTableBeauty";
+
+export default async function DashboardLayout() {
+	// const getData = await getStockPrice("EUNL.DE");
+	// console.log("🚀 ~ DashboardLayout ~ getData:", getData);
+	//   Odpowiedź z API dla EUNL.DE: {
+	//   'Global Quote': {
+	//     '01. symbol': 'EUNL.DE',
+	//     '02. open': '113.1450',
+	//     '03. high': '113.4950',
+	//     '04. low': '112.7700',
+	//     '05. price': '113.0050',
+	//     '06. volume': '518741',
+	//     '07. latest trading day': '2026-01-22',
+	//     '08. previous close': '112.2450',
+	//     '09. change': '0.7600',
+	//     '10. change percent': '0.6771%'
+	//   }
+	// }
+	return (
+		<div className="min-h-screen bg-slate-50 flex">
+			{/* Main Content */}
+			<main className="flex-1 p-8">
+				<header className="flex justify-between items-center mb-10">
+					<h1 className="text-3xl font-extrabold text-slate-800">
+						Przegląd Aktywów
+					</h1>
+					<div className="flex gap-4">
+						<span className="p-2 bg-green-100 text-green-700 rounded-full text-sm font-bold">
+							+2.4% Today
+						</span>
+					</div>
+				</header>
+				{/* Tutaj wskoczyłyby Twoje karty z wykresami */}
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+					<div className="h-64 bg-white rounded-xl shadow-sm border border-slate-200 p-4">
+						{/* Tu byłby wykres kołowy alokacji */}
+					</div>
+					<div className="h-64 bg-white rounded-xl shadow-sm border border-slate-200 p-4">
+						{/* Tu byłyby kursy Chevron/Meta */}
+					</div>
+				</div>
+				<div className="mt-5 text-slate-700">
+					<p>herer</p>
+					<AssetsTable assets={mockAssets} />
+				</div>
+				<div className="mt-5 text-slate-700"></div>
+				<div className="mt-5 text-slate-700">
+					<PortfolioTableBeauty assets={mockAssets} />
+				</div>
+
+				<div className="mt-5">
+					<ComparisonBars assets={mockAssets} />
+				</div>
+			</main>
+		</div>
+	);
+}

67c2efaa26c0aee652eab2f413e94741c03a0058
Greg Sypek <greg.sypek@gmail.com>
2026-02-17 18:51:10 +0100

    Display assets with some values only - PortfolioChart

--- components/PortfolioCharts.tsx
+++ components/PortfolioCharts.tsx
@@ -6,15 +6,18 @@ interface Props {
 }
 
 export default function PortfolioCharts({ data }: Props) {
+	const filteredData = data.filter((x) => x.weight > 0);
+
+	console.log("🚀 ~ PortfolioCharts ~ data:", data);
 	return (
 		<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
 			<PortfolioPieChart
-				data={data}
+				data={filteredData}
 				title="Target Strategy"
 				dataKey="weight"
 			/>
 			<PortfolioPieChart
-				data={data}
+				data={filteredData}
 				title="Current Allocation"
 				dataKey="actualPercentage"
 			/>

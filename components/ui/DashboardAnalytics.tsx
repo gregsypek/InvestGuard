f35a619831a85b48ab5fa3f938442778bcb0cd97
Greg Sypek <greg.sypek@gmail.com>
2026-03-23 19:55:34 +0100

    Add new link to edit targets in portfolio - from  dashboard

--- components/ui/DashboardAnalytics.tsx
+++ components/ui/DashboardAnalytics.tsx
@@ -1,15 +1,25 @@
 "use client";
 
-import { ArrowRightCircle, ChartArea, Plus } from "lucide-react";
+import {
+	ArrowRightCircle,
+	ChartArea,
+	ListOrdered,
+	PieChart,
+	Plus,
+	TableProperties,
+} from "lucide-react";
 import { CategoryStatus, PortfolioWithAssets } from "@/lib/types";
 
 import AddButton from "./AddButton";
 import AssetLedger from "../AssetLedgerTable";
+import { Button } from "./button";
 import Link from "next/link";
 import PortfolioCharts from "../PortfolioCharts";
 import React from "react";
 import RecentActivityCard from "./assets/RecentActivityCard";
+import { SectionHeader } from "../shared/SectionHeader";
 import StrategyHealthTable from "@/app/portfel/components/StrategyHealthTable";
+import { SubHeader } from "../shared/SubHeader";
 import { useMemo } from "react";
 import { useSearchParams } from "next/navigation";
 
@@ -51,22 +61,20 @@ const DashboardAnalytics = ({
 			<div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
 				<div className="lg:col-span-2 space-y-10">
 					<section>
-						<div className="flex justify-between items-center mb-6">
-							<h2 className="text-xl font-bold tracking-tight  flex items-center gap-2">
-								<ArrowRightCircle className="h-5 w-5 text-primary" />
-								Przewodnik rebalansowania
-							</h2>
-						</div>
+						<SectionHeader
+							title="Przewodnik rebalansowania"
+							icon={ArrowRightCircle}
+						/>
 						<StrategyHealthTable data={portfolioStatus} />
 					</section>
 				</div>
 
 				{/* SIDEBAR: Recent Assets (Minimalist style) */}
-				<aside className="space-y-6">
+				<aside className="space-y-6 bg-background">
 					<div className="flex justify-between items-center px-1">
-						<h2 className="text-lg font-bold flex items-center gap-2">
+						<h3 className="text-md font-bold flex items-center gap-2">
 							Ostatnie aktywa
-						</h2>
+						</h3>
 						<AddButton className="h-8 px-3 text-xs">
 							<Link
 								href={`/dashboard/${portfolio.id}/add-asset`}
@@ -81,7 +89,7 @@ const DashboardAnalytics = ({
 						{/* EN: Check if there are any assets to display */}
 						{/* UI: Sprawdzenie, czy lista aktywów nie jest pusta */}
 						{assets.length === 0 ? (
-							<div className="flex flex-col items-center justify-center p-8 border border-dashed border-border2 rounded-xl bg-card/30 text-center space-y-3">
+							<div className="flex flex-col items-center justify-center p-8 border border-dashed border-border rounded-xl bg-card/30 text-center space-y-3">
 								<div className="space-y-4">
 									<p className="text-sm font-medium">Brak aktywów</p>
 									<p className="text-xs text-muted-foreground leading-relaxed">
@@ -105,20 +113,58 @@ const DashboardAnalytics = ({
 					</div>
 				</aside>
 			</div>
-			<section>
-				<div className="flex justify-between items-center mb-6">
-					<h2 className="text-xl font-bold tracking-tight  flex items-center gap-2">
-						<ChartArea className="h-5 w-5 text-primary" />
-						Strategia Alokacji
-					</h2>
+			<section className="pt-8 border-t border-border">
+				<div className="flex justify-between">
+					<SectionHeader icon={ChartArea} title="Strategia Alokacji" />
+					<AddButton>
+						<Link
+							href={`/portfolios/edit/${portfolio.id}`}
+							className="flex items-center gap-2"
+						>
+							Edytuj cele
+						</Link>
+					</AddButton>
 				</div>
-				<PortfolioCharts data={portfolioStatus} />
+				<SubHeader
+					title="Wykresy kołowe"
+					description="Wizualizacja modelowego portfela  w zestawieniu z rzeczywistym stanem posiadania."
+					icon={PieChart}
+				/>
+
+				<div className="mx-6 py-2">
+					<PortfolioCharts data={portfolioStatus} />
+				</div>
 			</section>
 			{/* --- BOTTOM SECTION: RICH ASSET TABLE --- */}
-			<AssetLedger
-				portfolio={portfolio}
-				allPortfoliosWithCash={allPortfoliosWithCash}
-			/>
+			<section className="pt-8 border-t border-border">
+				<div className="flex justify-between">
+					<SectionHeader
+						icon={ListOrdered}
+						title="Szczegółowy Rejestr
+					Aktywów"
+					/>
+
+					<AddButton>
+						<Link
+							href={`/dashboard/${portfolio.id}/add-asset`}
+							className="flex items-center gap-2"
+						>
+							<Plus className="h-4 w-4" /> Nowe Aktywo
+						</Link>
+					</AddButton>
+				</div>
+				<SubHeader
+					title="Lista pozycji z portfela"
+					description="Lista pozycji z uwzględnieniem średniej ceny zakupu, liczby jednostek oraz skumulowanego wyniku P&L."
+					icon={TableProperties}
+				/>
+				<div className="mx-6 py-2">
+					<AssetLedger
+						portfolio={portfolio}
+						allPortfoliosWithCash={allPortfoliosWithCash}
+					/>
+				</div>
+			</section>
 		</div>
 	);
 };

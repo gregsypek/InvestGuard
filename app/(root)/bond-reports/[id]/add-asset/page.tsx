a839182c1b963420b8be911b31fcd045715bc0c4
Greg Sypek <greg.sypek@gmail.com>
2026-05-22 11:35:45 +0200

    feat: enhance AddBondPage with no portfolios state handling and add described headers

--- app/(root)/bond-reports/[id]/add-asset/page.tsx
+++ app/(root)/bond-reports/[id]/add-asset/page.tsx
@@ -1,8 +1,15 @@
+import { FileText, Form, LibrarySquareIcon } from "lucide-react";
+
+import AddAssetForm from "@/components/ui/assets/AddAssetForm";
 import AddBondForm from "@/components/ui/assets/AddBondForm";
 import { BondHeader } from "@/components/BondHeader";
-import { LibrarySquareIcon } from "lucide-react";
+import { BondImporter } from "@/components/ui/BondImporter";
+import PortfolioEmptyState from "@/components/PortfolioEmptyState";
+import { SectionHeader } from "@/components/shared/SectionHeader";
+import { SubHeader } from "@/components/shared/SubHeader";
 import { getBondsData } from "@/lib/actions/bond-actions";
 import { notFound } from "next/navigation";
+
 interface Props {
 	params: Promise<{ id: string }>;
 }
@@ -11,9 +18,11 @@ export default async function AddBondPage({ params }: Props) {
 	const { id } = await params;
 	const data = await getBondsData(id); // Wywołujesz to samo - Next.js to zoptymalizuje
 	if (!data) return notFound();
-
+	if (!id) {
+		return <PortfolioEmptyState variant="PORTFOLIOS" />;
+	}
 	return (
-		<div className="p-6 px-8 space-y-10 pb-20">
+		<div className="space-y-10 pb-20">
 			<BondHeader
 				title="Moje Obligacje"
 				portfolioName={data.portfolioName}
@@ -21,13 +30,38 @@ export default async function AddBondPage({ params }: Props) {
 				totalBonds={data.bonds.length}
 				backHref={`/bond-reports/${id}`}
 			/>
-			<div className="flex justify-between items-center mb-6">
+			{/* <div className="flex justify-between items-center mb-6">
 				<h2 className="text-xl font-bold tracking-tight  flex items-center gap-2">
 					<LibrarySquareIcon className="h-5 w-5 text-primary" />
-					{/* Formularz dodawania obligacji */}
 				</h2>
 			</div>
-			<AddBondForm portfolioId={id} />
+			<AddBondForm portfolioId={id} /> */}
+			<section className="pt-8 border-t border-border">
+				<div className="flex justify-between">
+					<SectionHeader
+						icon={FileText}
+						title="Import obligacji z raportu - PKO BP"
+					/>
+				</div>
+				<SubHeader
+					title="Automatyczne dodawanie obligacji"
+					description="Wygeneruj raport w banku PKO i zaimportuj go tutaj, aby szybko dodać swoje obligacje i przeliczyć aktualne saldo"
+					icon={Form}
+				/>
+				<div className="mx-6 py-2">
+					<BondImporter portfolioId={id} />{" "}
+				</div>
+			</section>
+			<section className="pt-8 border-t border-border">
+				<div className="flex justify-between">
+					<SectionHeader
+						icon={LibrarySquareIcon}
+						title="Ręczne dodawanie aktywów"
+					/>
+				</div>
+
+				<AddBondForm portfolioId={id} />
+			</section>
 		</div>
 	);
 }
